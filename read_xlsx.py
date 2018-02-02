80143ae41f434ac5464df438631fb267c31dee21
MAURO <merchan.ber.mau@hotmail.com>
2018-02-02 12:01:13 -0500

    manejo de sql con dataframe

--- read_xlsx.py
+++ read_xlsx.py
@@ -1,8 +1,17 @@
 #importar pandas
 import pandas as pd
 
+#importar sqldf para manejar sql
+from pandasql import sqldf
+
 #leer el archivo y la hoja
 df=pd.read_excel(open('archivo.xlsx','rb'), sheet_name='Sheet1')
 
+pysqldf = lambda q: sqldf(q, globals())
+
 #presentar lo que se leyo del .xlsx
-print(df).
+print(df)
+
+#manejo de sql con dataframe
+print (pysqldf("SELECT id,sum(valores) as saldo_tl,count(*) as n_multas,min(dias) as min_dia,max(dias) as max_dia  FROM df group by id;").head())
+
