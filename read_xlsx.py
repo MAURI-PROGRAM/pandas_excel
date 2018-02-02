e7dcca9a60112138a2518dd8bd88f576c05dde99
MAURO <merchan.ber.mau@hotmail.com>
2018-02-02 17:27:57 -0500

    consultas estadisticas con pandasl y describe dataframe

--- read_xlsx.py
+++ read_xlsx.py
@@ -5,14 +5,17 @@ import pandas as pd
 from pandasql import sqldf
 
 #leer el archivo y la hoja
-df=pd.read_excel(open('Sistema de cobro 2-01-2018.xlsx','rb'), sheet_name='Asignacion')
+df=pd.read_excel(open('archivo.xlsx','rb'), sheet_name='Sheet1')
 
 pysqldf = lambda q: sqldf(q, globals())
 
 
 #manejo de sql con dataframe
-print(pysqldf("SELECT SUBCAMPANAPORCLIENTE as fase,sum(SALDO)as valor,count(*) as asignados  FROM df group by SUBCAMPANAPORCLIENTE order by valor desc;"))
-print(pysqldf("""SELECT  CASE 
+consulta1=pysqldf("SELECT SUBCAMPANAPORCLIENTE as fase,sum(SALDO)as valor,count(*) as asignados  FROM df group by SUBCAMPANAPORCLIENTE order by valor desc;")
+
+consulta2=pysqldf("SELECT count(DISTINCT IDENTIFICACION_DEUDOR) as CANTIDAD,SUM(SALDO) as MONTO_TOTAL   FROM df ;")
+
+consulta3=pysqldf("""SELECT  CASE 
 WHEN DIAS_MORA<=30 THEN 'DE 0 A 30 DIAS'
 WHEN DIAS_MORA>30 and DIAS_MORA<=60 THEN 'DE 31 A 60 DIAS'
 WHEN DIAS_MORA>60 and DIAS_MORA<=90 THEN 'DE 61 A 90 DIAS'
@@ -22,7 +25,13 @@ WHEN DIAS_MORA>270 and DIAS_MORA<=360 THEN 'DE 270 A 360 DIAS'
 WHEN DIAS_MORA>360 and DIAS_MORA<=720 THEN 'DE 360 A 720 DIAS'
 WHEN DIAS_MORA>720 and DIAS_MORA THEN 'MAYOR A 720 DIAS'
 ELSE  'OTRO RANGO'
-END  as altura_mora, sum(SALDO)as valor,count(*) as asignados  FROM df group by altura_mora order by valor desc;"""))
+END  as altura_mora, sum(SALDO)as valor,count(*) as asignados  FROM df group by altura_mora order by valor desc;""")
 
 #estadisticos con dataframe
-print(df[['SALDO','DIAS_MORA','CODIGO_CONDICION']].describe())
+consulta4=df[['SALDO','DIAS_MORA','CODIGO_CONDICION']].describe()
+
+print(consulta1)
+print(consulta2)
+print(consulta3)
+print(consulta4)
+
